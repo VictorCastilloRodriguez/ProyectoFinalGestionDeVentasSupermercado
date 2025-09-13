b7b56378df3094c07b549e71f1fefd1c1767b8fa
victorcastillo <jade.tillo86@gmail.com>
2025-09-13 20:21:17 +0200

    metodos Sucursal

--- src/main/java/com/example/ProyectoFinalGestionDeVentasSupermercado/controller/SucursalController.java
+++ src/main/java/com/example/ProyectoFinalGestionDeVentasSupermercado/controller/SucursalController.java
@@ -1,20 +1,47 @@
 package com.example.ProyectoFinalGestionDeVentasSupermercado.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.ProyectoFinalGestionDeVentasSupermercado.model.Sucursal;
+import com.example.ProyectoFinalGestionDeVentasSupermercado.service.SucursalService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/sucursales")
 public class SucursalController {
 
-    //Añadir sucursal
+    @Autowired
+    private SucursalService sucursalService;
 
+    //Crear sucursal
+    @PostMapping
+    public ResponseEntity<Sucursal> crearSucursal(@Valid @RequestBody Sucursal sucursal) {
+        Sucursal nuevaSucursal = sucursalService.crearSucursal(sucursal);
+        return ResponseEntity.status(201).body(nuevaSucursal);
+    }
 
     //Actualizar sucursal
+    @PutMapping("/{id}")
+    public ResponseEntity<Sucursal> actualizarSucursal(@PathVariable Long id, @Valid @RequestBody Sucursal sucursalActualizada) {
+        Sucursal sucursalFinal = sucursalService.actualizarSucursal(id, sucursalActualizada);
+        return ResponseEntity.ok(sucursalFinal);
+    }
 
+    // Eliminar sucursal
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> eliminarSucursal(@PathVariable Long id) {
+        sucursalService.eliminarSucursal(id);
+        return ResponseEntity.noContent().build();
+    }
 
-    //Eliminar sucursal
-
-
-    //Listar sucursales
+    // Listar sucursales
+    @GetMapping
+    public ResponseEntity<Map<Long, Sucursal>> listarSucursales() {
+        Map<Long, Sucursal> sucursalesMap = sucursalService.listarSucursales();
+        return ResponseEntity.ok(sucursalesMap);
+    }
 }
+
--- src/main/java/com/example/ProyectoFinalGestionDeVentasSupermercado/service/SucursalService.java
+++ src/main/java/com/example/ProyectoFinalGestionDeVentasSupermercado/service/SucursalService.java
@@ -1,4 +1,46 @@
 package com.example.ProyectoFinalGestionDeVentasSupermercado.service;
 
+import com.example.ProyectoFinalGestionDeVentasSupermercado.exception.SucursalNotFoundException;
+import com.example.ProyectoFinalGestionDeVentasSupermercado.model.Sucursal;
+import com.example.ProyectoFinalGestionDeVentasSupermercado.repository.SucursalRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Service
 public class SucursalService {
+    @Autowired
+    private SucursalRepository sucursalRepository;
+
+    // Crear sucursal
+    public Sucursal crearSucursal(Sucursal sucursal) {
+        return sucursalRepository.save(sucursal);
+    }
+
+    // Actualizar sucursal
+    public Sucursal actualizarSucursal(Long id, Sucursal sucursalActualizada) {
+        Sucursal sucursal = sucursalRepository.findById(id)
+                .orElseThrow(() -> new SucursalNotFoundException("Sucursal con id: " + id + "no encontrada"));
+
+        sucursal.setNombreSucursal(sucursalActualizada.getNombreSucursal());
+        sucursal.setDireccion(sucursalActualizada.getDireccion());
+        return sucursalRepository.save(sucursal);
+    }
+
+    // Listar sucursal
+    public Map<Long, Sucursal> listarSucursales() {
+        return sucursalRepository.findAll().stream()
+                .collect(Collectors.toMap(Sucursal::getId, sucursal -> sucursal));
+    }
+
+    // Eliminar sucursal
+    public void eliminarSucursal(Long id) {
+        if (!sucursalRepository.existsById(id)) {
+            throw new SucursalNotFoundException("Sucursal con id: " + id + " no encontrada");
+        }
+        sucursalRepository.deleteById(id);
+    }
 }
+
