ae505d616ded9d15a688063727b1e5317d4d54a4
JuanMarti <previlsa@previlsa.com>
2025-09-30 11:45:43 +0200

    suprimir clases filtros

--- src/main/java/com/example/ProyectoFinalGestionDeVentasSupermercado/controller/EstadisticasController.java
+++ src/main/java/com/example/ProyectoFinalGestionDeVentasSupermercado/controller/EstadisticasController.java
@@ -1,12 +1,16 @@
 package com.example.ProyectoFinalGestionDeVentasSupermercado.controller;
 
+import com.example.ProyectoFinalGestionDeVentasSupermercado.dto.IngresoPorSucursalDto;
 import com.example.ProyectoFinalGestionDeVentasSupermercado.dto.ProductoDto;
+import com.example.ProyectoFinalGestionDeVentasSupermercado.dto.ProductoMasVendidoDto;
 import com.example.ProyectoFinalGestionDeVentasSupermercado.service.EstadisticasService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/estadisticas")
@@ -18,6 +22,22 @@ public class EstadisticasController {
     @GetMapping("/producto-mas-vendido")
     public ResponseEntity<ProductoDto> productoMasVendido() {
         return ResponseEntity.ok(estadisticasService.obtenerProductoMasVendido());
+    }
+
+    @GetMapping("/total-ingresos")
+    public ResponseEntity<String> getTotalIngresosVenta() {
+        Double total = estadisticasService.obtenerTotalIngresos();
+        return ResponseEntity.ok("El total de ingresos del supermercado es: " + total + "€.");
+    }
+
+    @GetMapping("/productos-mas-vendidos")
+    public ResponseEntity<List<ProductoMasVendidoDto>> getProductosMasVendidos() {
+        return ResponseEntity.ok(estadisticasService.listarProductosMasVendidos());
+    }
+
+    @GetMapping("/ingresos-por-sucursal")
+    public ResponseEntity<List<IngresoPorSucursalDto>> getIngresosPorSucursal() {
+        return ResponseEntity.ok(estadisticasService.obtenerTotalIngresosPorSucursal());
     }
 }
 
